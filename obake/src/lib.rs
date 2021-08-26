fd869929d550868e16f5773880ec8ca85117d30d
Nathan Corbyn <me@nathancorbyn.com>
2021-08-26 14:33:54 +0100

    Add some repo boilerplate

--- obake/src/lib.rs
+++ obake/src/lib.rs
@@ -1,14 +1,4 @@
 #![deny(clippy::all, clippy::pedantic)]
-// Ignored clippy lints
-#![allow(clippy::used_underscore_binding, clippy::wildcard_in_or_patterns)]
-// Ignored clippy_pedantic lints
-#![allow(
-    clippy::cast_possible_truncation,
-    clippy::doc_markdown,
-    clippy::similar_names,
-    clippy::unseparated_literal_suffix,
-    clippy::wildcard_imports
-)]
 #![no_std]
 
 pub use obake_macros::versioned;
